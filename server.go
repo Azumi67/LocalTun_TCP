e1a46b03893a6a50f8ae9a9d6dc7eaa8c430953c
Azumi <azumitan67@gmail.com>
2024-06-18 01:25:53 -0700

    Update server.go

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	serverPort := flag.Int("server-port", 5555, "Server port")
+	serverPort := flag.Int("server-port", 800, "Server port")
 	tunIP := flag.String("server-private", "10.0.0.1", "Kharej IP address")
 	iranIP := flag.String("client-private", "10.0.0.2", "Client IP address")
 	subnetMask := flag.String("subnet", "24", "Subnet mask (24 or 64)")
