85bf657f4fb7750686def91ee007ac3be86e29ba
Azumi <azumitan67@gmail.com>
2024-06-18 01:25:23 -0700

    Update client.go

--- client.go
+++ client.go
@@ -9,9 +9,9 @@ import (
 
 func main() {
 	kharejAddr := flag.String("server-addr", "SERVER_IP", "Server IP address")
-	serverPort := flag.Int("server-port", 5555, "Server port")
-	tunIP := flag.String("iran-private", "10.0.0.2", "TUN IP address")
-	kharejTunIP := flag.String("kharej-private", "10.0.0.1", "Server TUN IP address")
+	serverPort := flag.Int("server-port", 800, "Server port")
+	tunIP := flag.String("client-private", "10.0.0.2", "TUN IP address")
+	kharejTunIP := flag.String("server-private", "10.0.0.1", "Server TUN IP address")
 	subnetMask := flag.String("subnet", "24", "Subnet mask (e.g., 24 or 64)")
 	tunName := flag.String("device", "tun0", "TUN device name")
 	secretKey := flag.String("key", "", "Secret key for authentication")
