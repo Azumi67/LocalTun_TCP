08b490101d6e86b89b583be4e7aec33959602a49
Azumi <azumitan67@gmail.com>
2024-06-23 12:07:49 -0700

    Update server.go

--- server.go
+++ server.go
@@ -3,32 +3,22 @@ package main
 import (
 	"flag"
 	"log"
+	"time"
 
 	"github.com/Azumi67/LocalTun_TCP/server"
 )
 
 func main() {
 	serverPort := flag.Int("server-port", 800, "Server port")
-	tunIP := flag.String("server-private", "10.0.0.1", "Kharej IP address")
-	iranIP := flag.String("client-private", "10.0.0.2", "Client IP address")
-	subnetMask := flag.String("subnet", "24", "Subnet mask (24 or 64)")
-	tunName := flag.String("device", "tun0", "TUN device name")
-	secretKey := flag.String("key", "", "Secret key for authentication")
-	mtu := flag.Int("mtu", 1500, "MTU for TUN device")
+	tunIP := flag.String("server-private", "2001:db8::1", "Kharej IP address")
+	iranIP := flag.String("client-private", "2001:db8::2", "Client IP address")
+	subnetMask := flag.String("subnet", "64", "Subnet mask (24 or 64)")
+	tunName := flag.String("device", "tun2", "TUN device name")
+	secretKey := flag.String("key", "azumi", "Secret key for authentication")
+	mtu := flag.Int("mtu", 1480, "MTU for TUN device")
+	verbose := flag.Bool("verbose", false, "Enable logging")
 
 	flag.Parse()
 
-	config := tunserver.kharejConfig{
-		ServerPort: *serverPort,
-		TunIP:      *tunIP,
-		IranIP:     *iranIP,
-		SubnetMask: *subnetMask,
-		TunName:    *tunName,
-		SecretKey:  *secretKey,
-		MTU:        *mtu,
-	}
-
-	log.Printf("Server with config Started: %+v\n", config)
-
-	tunserver.serverSide(config)
+	tunserver.Run(*serverPort, *tunIP, *iranIP, *subnetMask, *tunName, *mtu, *secretKey, *verbose)
 }
