986ea56b2898bae2f81620905b49907122094239
Azumi <azumitan67@gmail.com>
2024-06-24 02:51:57 -0700

    Update server.go

--- server.go
+++ server.go
@@ -2,78 +2,96 @@ package main
 
 import (
 	"flag"
-	"log"
-        "github.com/Azumi67/LocalTun_TCP/server_smux"
-	"github.com/Azumi67/LocalTun_TCP/server"
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
 	"github.com/songgao/water"
+	"github.com/Azumi67/LocalTun_TCP/tcp_no_delay_server"
+	"github.com/Azumi67/LocalTun_TCP/server_smux"
+	"github.com/Azumi67/LocalTun_TCP/server"
 )
 
+var log = logrus.New()
 
 func main() {
-	serverPort := flag.Int("server-port", 8000, "Server port")
-	tunIP := flag.String("server-private", "2001:db8::1", "Server TUN IP address")
-	clientIP := flag.String("client-private", "2001:db8::2", "Client TUN IP address")
-	subnetMask := flag.String("subnet", "64", "Subnet mask (24 or 64)")
+	serverPort := flag.Int("server-port", 800, "Server port")
+	serverTunIP := flag.String("server-private", "2001:db8::1", "Server Private IP address")
+	clientTunIP := flag.String("client-private", "2001:db8::2", "Client Private IP address")
+	subnetMask := flag.String("subnet", "64", "Subnet mask (e.g: 24 or 64)")
 	tunName := flag.String("device", "tun2", "TUN device name")
 	secretKey := flag.String("key", "azumi", "Secret key for authentication")
 	mtu := flag.Int("mtu", 1480, "MTU for TUN device")
 	verbose := flag.Bool("verbose", false, "Enable logging")
-	useSmux := flag.Bool("smux", false, "Enable smux multiplexing")
+	useSmux := flag.Bool("smux", false, "Enable SMUX multiplexing")
 
 	flag.Parse()
 
-	config := water.Config{
-		DeviceType: water.TUN,
+	if *verbose {
+		log.SetLevel(logrus.DebugLevel)
+		log.SetOutput(os.Stdout)
+	} else {
+		log.SetLevel(logrus.InfoLevel)
 	}
-	config.Name = *tunName
-	tun, err := water.New(config)
+
+	tun, err := tunserver.Run(*serverTunIP, *clientTunIP, *subnetMask, *tunName, *mtu)
 	if err != nil {
-		log.Fatalf("Couldn't create TUN device: %v", err)
+		log.Fatalf("failed to set up TUN: %v", err)
 	}
 	defer tun.Close()
 
-	tunUp(tun, *tunIP, *clientIP, *subnetMask, *mtu, *tunName)
-
-	if *useSmux {
-		err := smuxserver.RunSmux(*serverPort, tun, *secretKey, *verbose)
+	for {
+		ln, err := net.Listen("tcp", fmt.Sprintf(":%d", *serverPort))
 		if err != nil {
-			log.Fatalf("Error running SMUX server: %v", err)
+			log.Errorf("failed to listen on port %d: %v", *serverPort, err)
+			time.Sleep(5 * time.Second)
+			continue
 		}
-	} else {
-		err := tunserver.Run(*serverPort, *tunIP, *clientIP, *subnetMask, *tunName, *secretKey, *mtu, *verbose)
+		defer ln.Close()
+
+		log.Infof("Server listening on port %d", *serverPort)
+
+		conn, err := ln.Accept()
 		if err != nil {
-			log.Fatalf("Error running TUN server: %v", err)
+			log.Warnf("failed to accept any connection: %v", err)
+			ln.Close()
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		log.Info("Client connected")
+
+		if *useSmux {
+			smuxserver.HandleSmux(conn, tun, *verbose)
+		} else {
+			tcpnodelayserver.noDelay(conn)
+			handleClient(conn, tun, *secretKey, *verbose)
 		}
 	}
 }
 
-func tunUp(tun *water.Interface, tunIP, clientIP, subnetMask string, mtu int, tunName string) {
-	if err := cmd("ip", "link", "set", "dev", tun.Name(), "up"); err != nil {
-		log.Fatalf("Couldn't bring up the TUN device: %v", err)
+func handleClient(conn net.Conn, tun *water.Interface, secretKey string, verbose bool) {
+	authBuf := make([]byte, len(secretKey))
+	if _, err := conn.Read(authBuf); err != nil {
+		log.Warnf("failed to read authentication key: %v", err)
+		return
 	}
 
-	if err := cmd("ip", "link", "set", "dev", tun.Name(), "mtu", fmt.Sprintf("%d", mtu)); err != nil {
-		log.Fatalf("Couldn't set MTU: %v", err)
+	if string(authBuf) != secretKey {
+		log.Warnf("Wrong authentication key")
+		return
 	}
 
-	if err := cmd("ip", "addr", "add", fmt.Sprintf("%s/%s", tunIP, subnetMask), "dev", tun.Name()); err != nil {
-		log.Fatalf("Couldn't assign private IP address to TUN device: %v", err)
-	}
+	clientToTun := make(chan []byte, 100)
+	tunToClient := make(chan []byte, 100)
 
-	if iPv6(clientIP) {
-		if err := cmd("ip", "-6", "route", "add", fmt.Sprintf("%s/128", clientIP), "dev", tun.Name()); err != nil {
-			log.Fatalf("Adding route for private IPv6 failed: %v", err)
-		}
-	} else {
-		if err := cmd("ip", "route", "add", fmt.Sprintf("%s/32", clientIP), "dev", tun.Name()); err != nil {
-			log.Fatalf("Adding route for private IPv4 failed: %v", err)
-		}
-	}
+	go fromClient(conn, tun, clientToTun, verbose)
+	go toTun(tun, clientToTun, verbose)
+	go fromTun(tun, tunToClient, verbose)
+	go toClient(conn, tunToClient, verbose)
 
-	if err := cmd("sh", "-c", "echo 1 > /proc/sys/net/ipv4/ip_forward"); err != nil {
-		log.Fatalf("Enabling IPv4 forwarding failed: %v", err)
-	}
-	if err := cmd("sh", "-c", "echo 1 > /proc/sys/net/ipv6/conf/all/forwarding"); err != nil {
-		log.Fatalf("Enabling IPv6 forwarding failed: %v", err)
-	}
+	select {}
 }
+//byebye
