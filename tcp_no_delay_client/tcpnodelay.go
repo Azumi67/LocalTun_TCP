a80a057da81280055a54ca67b91d635734f1657a
Azumi <azumitan67@gmail.com>
2024-06-24 03:48:08 -0700

    Update tcpnodelay.go

--- tcp_no_delay_client/tcpnodelay.go
+++ tcp_no_delay_client/tcpnodelay.go
@@ -2,29 +2,11 @@ package tcpnodelayclient
 
 import (
 	"net"
-	"syscall"
-	"time"
 )
 
-func noDelay(conn *net.TCPConn) error {
-	err := conn.noDelay(true)
-	if err != nil {
-		return err
+func NoDelay(conn net.Conn, tcpNoDelay bool) error {
+	if tcpConn, ok := conn.(*net.TCPConn); ok {
+		return tcpConn.SetNoDelay(tcpNoDelay)
 	}
-
-	err = conn.SetKeepAlive(true)
-	if err != nil {
-		return err
-	}
-
-	err = conn.SetKeepAlivePeriod(30 * time.Second)
-	if err != nil {
-		return err
-	}
-
-	if err := syscall.SetsockoptInt(conn.SocketConn().Sysfd, syscall.IPPROTO_TCP, syscall.TCP_FASTOPEN, 1); err != nil {
-		return err
-	}
-
 	return nil
 }
