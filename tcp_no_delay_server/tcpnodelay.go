af62a018cc85e0ab7b7c4f9c6fe6ec5847ca73c8
Azumi <azumitan67@gmail.com>
2024-06-24 03:21:30 -0700

    Update tcpnodelay.go

--- tcp_no_delay_server/tcpnodelay.go
+++ tcp_no_delay_server/tcpnodelay.go
@@ -2,17 +2,60 @@ package tcpnodelayserver
 
 import (
 	"net"
+	"github.com/songgao/water"
+	"sync"
+	"github.com/sirupsen/logrus"
+	"github.com/Azumi67/LocalTun_TCP/server"
 )
 
-func noDelay(conn net.Conn) {
-	tcpConn, ok := conn.(*net.TCPConn)
-	if !ok {
-		log.Warnf("Setting up TCP nodelay failed")
+var log = logrus.New()
+
+func HandleClient(conn net.Conn, tun *water.Interface, secretKey string, verbose bool, tcpNoDelay bool) {
+	if tcpConn, ok := conn.(*net.TCPConn); ok {
+		tcpConn.SetNoDelay(tcpNoDelay)
+	}
+
+	buff := make([]byte, len(secretKey))
+	if _, err := conn.Read(buff); err != nil {
+		log.Warnf("Couldn't read authentication key: %v", err)
+		conn.Close()
 		return
 	}
 
-	err := tcpConn.noDelay(true)
-	if err != nil {
-		log.Warnf("failed to set up TCP nodelay: %v", err)
+	if string(buff) != secretKey {
+		log.Warn("Wrong authentication key")
+		conn.Close()
+		return
 	}
+
+	clientToTun := make(chan []byte, 100)
+	tunToClient := make(chan []byte, 100)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		tunserver.FromClient(conn, tun, clientToTun, verbose)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		tunserver.ToTun(tun, clientToTun, verbose)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		tunserver.FromTun(tun, tunToClient, verbose)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		tunserver.ToClient(conn, tunToClient, verbose)
+	}()
+
+	wg.Wait()
 }
