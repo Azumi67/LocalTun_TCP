43fcf4461f4f53ce1d2bce566dcb9df72107d8e3
Azumi <azumitan67@gmail.com>
2024-06-25 03:15:54 -0700

    Update tunserverpackage.go

--- server/tunserverpackage.go
+++ server/tunserverpackage.go
@@ -1,130 +1,51 @@
 package tunserver
 
 import (
-	"encoding/binary"
+	"fmt"
+	"os/exec"
+	"net"
 	"github.com/sirupsen/logrus"
 	"github.com/songgao/water"
-	"net"
-	"sync"
 )
 
 var log = logrus.New()
 
-func HandleClient(conn net.Conn, tun *water.Interface, secretKey string, verbose bool) {
-	buff := make([]byte, len(secretKey))
-	if _, err := conn.Read(buff); err != nil {
-		log.Warnf("Couldn't read authentication key: %v", err)
-		conn.Close()
-		return
+func TunUp(tun *water.Interface, tunIP, clientTunIP, subnetMask string, mtu int, tunName string) {
+	if err := cmd("ip", "link", "set", "dev", tun.Name(), "up"); err != nil {
+		log.Fatalf("Couldn't bring up the TUN device: %v", err)
 	}
 
-	if string(buff) != secretKey {
-		log.Warn("Wrong authentication key")
-		conn.Close()
-		return
+	if err := cmd("ip", "link", "set", "dev", tun.Name(), "mtu", fmt.Sprintf("%d", mtu)); err != nil {
+		log.Fatalf("Couldn't set MTU: %v", err)
 	}
 
-	clientToTun := make(chan []byte, 100)
-	tunToClient := make(chan []byte, 100)
+	if err := cmd("ip", "addr", "add", fmt.Sprintf("%s/%s", tunIP, subnetMask), "dev", tun.Name()); err != nil {
+		log.Fatalf("Couldn't assign private IP address to TUN device: %v", err)
+	}
 
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		fromClient(conn, tun, clientToTun, verbose)
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		toTun(tun, clientToTun, verbose)
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		fromTun(tun, tunToClient, verbose)
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		toClient(conn, tunToClient, verbose)
-	}()
-
-	wg.Wait()
-}
-
-func fromClient(conn net.Conn, tun *water.Interface, clientToTun chan []byte, verbose bool) {
-	for {
-		pcktLength := make([]byte, 2)
-		if _, err := conn.Read(pcktLength); err != nil {
-			log.Warnf("Couldn't read packet length from client: %v", err)
-			close(clientToTun) 
-			return
+	if isIPv6(clientTunIP) {
+		if err := cmd("ip", "-6", "route", "add", fmt.Sprintf("%s/128", clientTunIP), "dev", tun.Name()); err != nil {
+			log.Fatalf("Adding route for private IPv6 failed: %v", err)
 		}
-
-		length := binary.BigEndian.Uint16(pcktLength)
-		buff := make([]byte, length)
-
-		_, err := data(conn, buff)
-		if err != nil {
-			log.Warnf("Couldn't read data from client: %v", err)
-			close(clientToTun) 
-			return
+	} else {
+		if err := cmd("ip", "route", "add", fmt.Sprintf("%s/32", clientTunIP), "dev", tun.Name()); err != nil {
+			log.Fatalf("Adding route for private IPv4 failed: %v", err)
 		}
+	}
 
-		clientToTun <- buff
+	if err := cmd("sh", "-c", "echo 1 > /proc/sys/net/ipv4/ip_forward"); err != nil {
+		log.Fatalf("Enabling IPv4 forwarding failed: %v", err)
+	}
+	if err := cmd("sh", "-c", "echo 1 > /proc/sys/net/ipv6/conf/all/forwarding"); err != nil {
+		log.Fatalf("Enabling IPv6 forwarding failed: %v", err)
 	}
 }
 
-func toTun(tun *water.Interface, clientToTun chan []byte, verbose bool) {
-	for {
-		buff, ok := <-clientToTun
-		if !ok {
-			return 
-		}
-		if _, err := tun.Write(buff); err != nil {
-			log.Warnf("Couldn't write to TUN device: %v", err)
-		}
-	}
+func cmd(command string, args ...string) error {
+	cmd := exec.Command(command, args...)
+	return cmd.Run()
 }
 
-func fromTun(tun *water.Interface, tunToClient chan []byte, verbose bool) {
-	for {
-		buff := make([]byte, 1500)
-		n, err := tun.Read(buff)
-		if err != nil {
-			log.Warnf("Couldn't read from TUN device: %v", err)
-			continue
-		}
-
-		packet := make([]byte, 2+n)
-		binary.BigEndian.PutUint16(packet[:2], uint16(n))
-		copy(packet[2:], buff[:n])
-
-		tunToClient <- packet
-	}
-}
-
-func toClient(conn net.Conn, tunToClient chan []byte, verbose bool) {
-	for {
-		packet, ok := <-tunToClient
-		if !ok {
-			return 
-		}
-		if _, err := conn.Write(packet); err != nil {
-			log.Warnf("Couldn't write to client: %v", err)
-			return
-		}
-	}
-}
-
-func data(conn net.Conn, buff []byte) (int, error) {
-	n, err := conn.Read(buff)
-	if err != nil {
-		return 0, err
-	}
-	return n, nil
+func isIPv6(ip string) bool {
+	return net.ParseIP(ip).To4() == nil
 }
