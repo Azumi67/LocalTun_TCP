e8abb70bbb7ba24b3eb1aff1b0376466841c0a73
Azumi <azumitan67@gmail.com>
2024-06-26 09:37:10 -0700

    Update heartbeat.go

--- heartbeat_client/heartbeat.go
+++ heartbeat_client/heartbeat.go
@@ -8,17 +8,15 @@ import (
 
 var log = logrus.New()
 
-func trueHeartbeat(conn net.Conn, interval int) {
-	go func() {
-		heartbeatTiktok := time.NewTicker(time.Duration(interval) * time.Second)
-		defer heartbeatTiktok.Stop()
+func trueHeartbeat(conn net.Conn, heartbeatInterval int) {
+	heartbeatTiktok := time.NewTicker(time.Duration(heartbeatInterval) * time.Second)
+	defer heartbeatTiktok.Stop()
 
-		for range heartbeatTiktok.C {
-			if _, err := conn.Write([]byte("ping")); err != nil {
-				log.Warnf("Sending heartbeat failed: %v", err)
-				conn.Close()
-				return
-			}
+	for range heartbeatTiktok.C {
+		if _, err := conn.Write([]byte("ping")); err != nil {
+			log.Warnf("Sending heartbeat failed: %v", err)
+			conn.Close()
+			return
 		}
-	}()
+	}
 }
