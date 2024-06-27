a16607707358fa557f7f95e9baacf53841f43336
Azumi <azumitan67@gmail.com>
2024-06-27 02:20:34 -0700

    Update smux.go

--- server_smux/smux.go
+++ server_smux/smux.go
@@ -1,11 +1,10 @@
 package serversmux
 
 import (
-	"github.com/sirupsen/logrus"
+	"net"
 	"github.com/songgao/water"
 	"github.com/xtaci/smux"
-	"net"
-	"github.com/Azumi67/LocalTun_TCP/server"
+	"github.com/sirupsen/logrus"
 )
 
 var log = logrus.New()
@@ -29,11 +28,82 @@ func HandleSmux(conn net.Conn, tun *water.Interface, verbose bool) {
 		clientToTun := make(chan []byte, 100)
 		tunToClient := make(chan []byte, 100)
 
-		go tunserver.FromClient(stream, tun, clientToTun, verbose)
-		go tunserver.ToTun(tun, clientToTun, verbose)
-		go tunserver.FromTun(tun, tunToClient, verbose)
-		go tunserver.ToClient(stream, tunToClient, verbose)
+		go fromClient(stream, tun, clientToTun, verbose)
+		go toTun(tun, clientToTun, verbose)
+		go fromTun(tun, tunToClient, verbose)
+		go toClient(stream, tunToClient, verbose)
 
 		select {}
 	}
 }
+
+func fromClient(conn net.Conn, tun *water.Interface, clientToTun chan []byte, verbose bool) {
+	for {
+		pcktLength := make([]byte, 2)
+		if _, err := conn.Read(pcktLength); err != nil {
+			log.Warnf("Couldn't read packet length from client: %v", err)
+			close(clientToTun)
+			return
+		}
+
+		length := binary.BigEndian.Uint16(pcktLength)
+		buff := make([]byte, length)
+
+		_, err := data(conn, buff)
+		if err != nil {
+			log.Warnf("Couldn't read data from client: %v", err)
+			close(clientToTun)
+			return
+		}
+
+		clientToTun <- buff
+	}
+}
+
+func toTun(tun *water.Interface, clientToTun chan []byte, verbose bool) {
+	for buff := range clientToTun {
+		if _, err := tun.Write(buff); err != nil {
+			log.Warnf("Couldn't write to TUN device: %v", err)
+		}
+	}
+}
+
+func fromTun(tun *water.Interface, tunToClient chan []byte, verbose bool) {
+	for {
+		buff := make([]byte, 1500)
+		n, err := tun.Read(buff)
+		if err != nil {
+			log.Warnf("Couldn't read from TUN device: %v", err)
+			continue
+		}
+
+		packet := make([]byte, 2+n)
+		binary.BigEndian.PutUint16(packet[:2], uint16(n))
+		copy(packet[2:], buff[:n])
+
+		tunToClient <- packet
+	}
+}
+
+func toClient(conn net.Conn, tunToClient chan []byte, verbose bool) {
+	for packet := range tunToClient {
+		_, err := conn.Write(packet)
+		if err != nil {
+			log.Warnf("Couldn't write to client: %v", err)
+			return
+		}
+	}
+}
+
+func data(conn net.Conn, buff []byte) (int, error) {
+	totalRead := 0
+	for totalRead < len(buff) {
+		n, err := conn.Read(buff[totalRead:])
+		if err != nil {
+			return totalRead, err
+		}
+		totalRead += n
+	}
+	return totalRead, nil
+}
+//some functions migrated from tunserver
