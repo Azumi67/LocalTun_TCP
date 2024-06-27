a038e95b774b4df9cd79d2aac99cb3cb0dbf4101
Azumi <azumitan67@gmail.com>
2024-06-27 02:17:56 -0700

    Update tunserverpackage.go

--- server/tunserverpackage.go
+++ server/tunserverpackage.go
@@ -1,9 +1,7 @@
 package tunserver
 
 import (
-	"encoding/binary"
 	"fmt"
-	"net"
 	"os/exec"
 	"github.com/sirupsen/logrus"
 	"github.com/songgao/water"
@@ -11,105 +9,46 @@ import (
 
 var log = logrus.New()
 
-func TunUp(tun *water.Interface, tunIP, clientTunIP, subnetMask string, mtu int, tunName string) {
+func TunUp(tunIP, clientTunIP, subnetMask string, mtu int, tunName string) (*water.Interface, error) {
+	config := water.Config{
+		DeviceType: water.TUN,
+	}
+	config.Name = tunName
+	tun, err := water.New(config)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't create TUN device: %v", err)
+	}
+
 	if err := cmd("ip", "link", "set", "dev", tun.Name(), "up"); err != nil {
-		log.Fatalf("Couldn't bring up the TUN device: %v", err)
+		return nil, fmt.Errorf("couldn't bring up the TUN device: %v", err)
 	}
 
 	if err := cmd("ip", "link", "set", "dev", tun.Name(), "mtu", fmt.Sprintf("%d", mtu)); err != nil {
-		log.Fatalf("Couldn't set MTU: %v", err)
+		return nil, fmt.Errorf("couldn't set MTU: %v", err)
 	}
 
 	if err := cmd("ip", "addr", "add", fmt.Sprintf("%s/%s", tunIP, subnetMask), "dev", tun.Name()); err != nil {
-		log.Fatalf("Couldn't assign private IP address to TUN device: %v", err)
+		return nil, fmt.Errorf("couldn't assign private IP address to TUN device: %v", err)
 	}
 
 	if iPv6(clientTunIP) {
 		if err := cmd("ip", "-6", "route", "add", fmt.Sprintf("%s/128", clientTunIP), "dev", tun.Name()); err != nil {
-			log.Fatalf("Adding route for private IPv6 failed: %v", err)
+			return nil, fmt.Errorf("adding route for private IPv6 failed: %v", err)
 		}
 	} else {
 		if err := cmd("ip", "route", "add", fmt.Sprintf("%s/32", clientTunIP), "dev", tun.Name()); err != nil {
-			log.Fatalf("Adding route for private IPv4 failed: %v", err)
+			return nil, fmt.Errorf("adding route for private IPv4 failed: %v", err)
 		}
 	}
 
 	if err := cmd("sh", "-c", "echo 1 > /proc/sys/net/ipv4/ip_forward"); err != nil {
-		log.Fatalf("Enabling IPv4 forwarding failed: %v", err)
+		return nil, fmt.Errorf("enabling IPv4 forwarding failed: %v", err)
 	}
 	if err := cmd("sh", "-c", "echo 1 > /proc/sys/net/ipv6/conf/all/forwarding"); err != nil {
-		log.Fatalf("Enabling IPv6 forwarding failed: %v", err)
+		return nil, fmt.Errorf("enabling IPv6 forwarding failed: %v", err)
 	}
-}
 
-func FromClient(conn net.Conn, tun *water.Interface, clientToTun chan []byte, verbose bool) {
-	for {
-		pcktLength := make([]byte, 2)
-		if _, err := conn.Read(pcktLength); err != nil {
-			log.Warnf("Couldn't read packet length from client: %v", err)
-			close(clientToTun)
-			return
-		}
-
-		length := binary.BigEndian.Uint16(pcktLength)
-		buff := make([]byte, length)
-
-		_, err := data(conn, buff)
-		if err != nil {
-			log.Warnf("Couldn't read data from client: %v", err)
-			close(clientToTun)
-			return
-		}
-
-		clientToTun <- buff
-	}
-}
-
-func ToTun(tun *water.Interface, clientToTun chan []byte, verbose bool) {
-	for buff := range clientToTun {
-		if _, err := tun.Write(buff); err != nil {
-			log.Warnf("Couldn't write to TUN device: %v", err)
-		}
-	}
-}
-
-func FromTun(tun *water.Interface, tunToClient chan []byte, verbose bool) {
-	for {
-		buff := make([]byte, 1500)
-		n, err := tun.Read(buff)
-		if err != nil {
-			log.Warnf("Couldn't read from TUN device: %v", err)
-			continue
-		}
-
-		packet := make([]byte, 2+n)
-		binary.BigEndian.PutUint16(packet[:2], uint16(n))
-		copy(packet[2:], buff[:n])
-
-		tunToClient <- packet
-	}
-}
-
-func ToClient(conn net.Conn, tunToClient chan []byte, verbose bool) {
-	for packet := range tunToClient {
-		_, err := conn.Write(packet)
-		if err != nil {
-			log.Warnf("Couldn't write to client: %v", err)
-			return
-		}
-	}
-}
-
-func data(conn net.Conn, buff []byte) (int, error) {
-	totalRead := 0
-	for totalRead < len(buff) {
-		n, err := conn.Read(buff[totalRead:])
-		if err != nil {
-			return totalRead, err
-		}
-		totalRead += n
-	}
-	return totalRead, nil
+	return tun, nil
 }
 
 func cmd(command string, args ...string) error {
@@ -123,3 +62,4 @@ func cmd(command string, args ...string) error {
 func iPv6(address string) bool {
 	return net.ParseIP(address).To4() == nil
 }
+//transfering it into serversmux
