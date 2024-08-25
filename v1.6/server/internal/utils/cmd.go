c56ae65a836eff4628db05a0c792e4a8142792a3
Azumi <azumitan67@gmail.com>
2024-08-24 22:02:50 -0700

    Create cmd.go

---
+++ v1.6/server/internal/utils/cmd.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"os/exec"
+)
+
+func Cmd(name string, args ...string) error {
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
