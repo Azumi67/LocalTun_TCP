7e0750b0fd2da624429e6e888dd46242bedf1b64
Azumi <azumitan67@gmail.com>
2024-09-01 02:42:24 -0700

    Create crypto.go

---
+++ 1.7/client/crypto.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+)
+
+func LoadPrivateKey(path string) (*rsa.PrivateKey, error) {
+	keyData, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	block, _ := pem.Decode(keyData)
+	if block == nil || block.Type != "RSA PRIVATE KEY" {
+		return nil, fmt.Errorf("invalid private key data")
+	}
+	return x509.ParsePKCS1PrivateKey(block.Bytes)
+}
