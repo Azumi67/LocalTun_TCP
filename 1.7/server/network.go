fee6dd0eb56ff424947267e29712b3dc65e8b9dc
Azumi <azumitan67@gmail.com>
2024-09-01 02:31:53 -0700

    Create network.go

---
+++ 1.7/server/network.go
@@ -0,0 +1,14 @@
+package utils
+
+import "net"
+
+func IPv6(ip string) bool {
+	return net.ParseIP(ip) != nil && net.ParseIP(ip).To4() == nil
+}
+
+func DefaultSubnet(ip string) string {
+	if IPv6(ip) {
+		return "64"
+	}
+	return "24"
+}
